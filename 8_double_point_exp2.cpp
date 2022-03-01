7edcf8a484c9f72c339afec5f45522d3ef53f7ca
Dylan1021 <a120053429@gmail.com>
2022-03-01 23:32:09 +0800

    Format the code

--- 8_double_point_exp2.cpp
+++ 8_double_point_exp2.cpp
@@ -9,19 +9,23 @@ const int N = 100010;
 int n;
 int a[N], s[N];
 
-int mian(){
+int mian()
+{
     cin >> n;
-    for(int i = 0 ; i < n ; i ++) cin >> a[i];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
 
-    int res = 0 ;
-    for(int i= 0 , j = 0 ; i < n; i++){
-        s[a[i]] ++;
-        while(s[a[i]] > 1){
-            s[a[j]] -- ;
-            j ++;
+    int res = 0;
+    for (int i = 0, j = 0; i < n; i++)
+    {
+        s[a[i]]++;
+        while (s[a[i]] > 1)
+        {
+            s[a[j]]--;
+            j++;
         }
 
-        res = max(res,i -j + 1);
+        res = max(res, i - j + 1);
     }
     cout << res << endl;
 
