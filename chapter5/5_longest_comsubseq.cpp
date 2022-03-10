51b2097c01484b0f9676275b551b6ce138e086fc
Dylan1021 <a120053429@gmail.com>
2022-03-10 09:49:41 +0800

    Update

---
+++ chapter5/5_longest_comsubseq.cpp
@@ -0,0 +1,26 @@
+// 最长公共子序列
+// 题目：897
+
+#include <iostream>
+#include <algorithm>
+
+const int N = 1010;
+
+int n,m;
+char a[N],b[N];
+int f[N][N];
+
+int main(){
+    scanf("%d%d",&n,&m);
+    scanf("%s%s",a+1,b+1);
+
+    for(int i = 1 ; i <= n ; i++)
+        for(int j = 1 ; j<= m ; j++){
+            f[i][j] = max(f[i-1][j],f[i][j-1]);
+            if(a[i] == b[j]) f[i][j] = max(f[i][j], f[i-1][j-1]+1);
+
+        }
+    
+    printf("%d\n",f[n][m]);
+    return 0;
+}
